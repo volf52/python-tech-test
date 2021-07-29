e7aa1d491979129d906feb0f48d177aed6f0592f
Arslan <rslnkrmt2552@gmail.com>
2021-07-29 10:01:40 +0300

    Add url counters and related routes

--- ushort/app.py
+++ ushort/app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException, Path
 from fastapi.requests import Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-from ushort.db import get_db, get_url_counter
+from ushort.db import get_url_count, get_url_freq, get_url_table, update_url_count
 from ushort.schemas import URLShortenRequest
 from ushort.shortener import generate_url_id, is_valid_url, validate_url_id
 
@@ -19,13 +19,15 @@ async def shorten_url(ushort: URLShortenRequest, req: Request):
             status_code=HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid URL"
         )
 
-    db = get_db()
+    url_table = get_url_table()
 
     url_id = generate_url_id(url)
-    db[url_id] = url
+    url_table[url_id] = url
 
     # Update counters
-    url_counter = get_url_counter()
+    update_url_count()
+
+    url_counter = get_url_freq()
     url_counter.update([url])
 
     return {"shortened_url": f"{req.url.scheme}://{req.url.netloc}/s/{url_id}"}
@@ -38,9 +40,9 @@ async def process_short_url(url_id: str = Path(...)):
     if msg is not None:
         raise HTTPException(status_code=HTTP_422_UNPROCESSABLE_ENTITY, detail=msg)
 
-    db = get_db()
+    url_table = get_url_table()
 
-    long_url = db.get(url_id)
+    long_url = url_table.get(url_id)
 
     if long_url is None:
         raise HTTPException(
@@ -49,3 +51,18 @@ async def process_short_url(url_id: str = Path(...)):
         )
 
     return long_url
+
+
+@app.get("/shortened_urls_count")
+async def url_count():
+    return get_url_count()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def show_most_popular():
+    cntr = get_url_freq()
+
+    most_popular_10 = cntr.most_common(n=10)
+    mp10_string = ", ".join(f"<a href={x[0]}>{x[0]}</a>" for x in most_popular_10)
+
+    return f"<p><strong>Most popular urls: </strong>{mp10_string}</p>"
--- ushort/db.py
+++ ushort/db.py
@@ -1,13 +1,28 @@
 from collections import Counter
 from functools import lru_cache
-from typing import Dict
+
+URL_TABLE_NAME = "url_mapping"
+COUNTER_KEY = "count"
 
 
 @lru_cache(maxsize=1)
-def get_db() -> Dict[str, str]:
-    return {}
+def get_db() -> dict:
+    return {COUNTER_KEY: 0, URL_TABLE_NAME: {}}
+
+
+def get_url_table():
+    return get_db()[URL_TABLE_NAME]
 
 
 @lru_cache(maxsize=1)
-def get_url_counter() -> Counter:
+def get_url_freq() -> Counter:
     return Counter()
+
+
+def update_url_count():
+    db = get_db()
+    db[COUNTER_KEY] += 1
+
+
+def get_url_count():
+    return get_db()[COUNTER_KEY]
